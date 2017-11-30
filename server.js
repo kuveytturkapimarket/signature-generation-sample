2364b192e64f9cab0e2f545a2c00b17dee8e392c
KTFintech <fintech@kuveytturk.com.tr>
2017-11-30 11:22:32 +0300

    add creation certificate on get request
    add creation certificate on get request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const crypto = require('crypto');
+const url = require('url');
 
 
 // parse application/x-www-form-urlencoded
@@ -16,34 +17,26 @@ app.use((req, res, next) => {
     /* Read in an OpenSSH/PEM *private* key */
     var privateKey =
         '-----BEGIN RSA PRIVATE KEY-----\n' +
-        'MIICXQIBAAKBgQCXeY6SwQjlvHJZHYXtiePL6Mwif+dilRfAzep7lYkZEqvTdsdR\n' +
-        '487+zxEyoQVxWTLeIiJLB98WzsPkQGW7UkZIJbqc8JhJcs7CKDVE2xxP92fWp1ot\n' +
-        'sxv5sitoVEnY66uzjZKp09bHGUyh9qwVwKvcmIRKQwrkiVl3S/6NyIhQfQIDAQAB\n' +
-        'AoGBAJH+EcE2Fdo+18M2Zke8l/aHSICT4DKu2jb1AGPei0XELiHhIFi4BWCnuL6g\n' +
-        'XXuS6ikYow7H5me4HyvWOXbi5GLUPWggVifyg+COU2H+RvyIVxsgAt4h3jmoUPC+\n' +
-        '+onPBJwSjZ7v5I3fE7HuDWE64mcxNlasrvOhlHHz5To6ikPhAkEA4KpvvxNZ2Y7K\n' +
-        'yNLO1xqUwjsbji7PSmFM+hI0kVIITeZTcdCpnKx12R7TxwOQM/nvsCCgSeXLvNbx\n' +
-        'mC/23u50RQJBAKyZ4THmK+HpGtBuiIGZ+W4ipTl2d9BIZtRBYt9gpsvkG6eYcdgL\n' +
-        '3GhhlLWThcePfOEzj5GSMvVB1hDEYTqh2tkCQAI1KovyILvmj0+R4r5yYGvYMNkg\n' +
-        '1KCifCMo8qNK7xH05XUE2+XSPAY6K1KVUmFwX06Xw3t2ap4cbptUPVTLexECQQCg\n' +
-        '0XOwsZx7jl1MjWClGNj+FEvMQaSbLYqaBB5isURYPz7BJloUieCgZSNRF5YTfY9G\n' +
-        'qWsAoK7YM7KHxuIs5jfpAkAkKFx10lV+K+g13rHENJNkQqCaBP0QhxBnNZAg9fKl\n' +
-        'Fi1edlPs1BRqCRrXYd66Pskpd/kz3bL6nymz8CzydSJb\n' +
-        '-----END RSA PRIVATE KEY-----';;
+        'MIICWgIBAAKBgF35LJRcbqFV8wtwYfVj2vaXMVF7YSRclf8z/7GsZGkjundmHfID\n' +
+        '+YJdEHX3MTfyp6+TuEwjobXrl+B3Uxm/1AnXC/LDsfAtrfFRTOukfynL8DHaOrh+\n' +
+        'ZlDEmM+TZJb3LM4FaoHJ2p0u9fnDwwyDLpiY7qKbpcXg4KpvsdYhSviPAgMBAAEC\n' +
+        'gYBDe2mt6GpxZN0xe7R8mQZmSdBaqPTrPalhKCAXoKpCbiu7DhO29W+5U8c8TxSD\n' +
+        'Dren97ZCxtubXeEkicudFAnZX4cGoCK6PZGZEMP6cYElbEOAXNy2Fifeyyg9QfOv\n' +
+        'dzckdy7cHPGqQPoN3QbsISWu74SmzwU5VS41A+DdYab4gQJBAKjqamet4riVnsUN\n' +
+        'S+jp1rw2/qelUxvHpxWt3H/L6x1imUY2s/DPEq0haxg2lXXfKUf3ZLevYcE38jKB\n' +
+        'IsIoTKECQQCOa9OKK1BUpJ42Dqvrm0rJFxh0UjYe+QVV4xpyk/8SfUI8Q2BLcRes\n' +
+        'Slew+SG5vJ5iYr+27BCUX9PZon2t/IcvAkAjnVlWBum4tYNGVOlecH9gK9BBlLAT\n' +
+        'OWNXRNMq9tnZ1kIu9bW23lI3wsYL0uZsdeMEU3se55Ga5arhTyz+9vRBAkBBMbq0\n' +
+        'FP/Av1ptBGpOf9fMFXZsSPXdV8DJgBwrNvQcUppvW6hI4IwloiJiMKjvjRgzkWCR\n' +
+        'K56rTPJ9PZSqgF6hAkBgXWfD/z+Dl+kxTKuKgIxCmBQWVON0uIEg/wr21uEqXPT0\n' +
+        'nhY6lVpEb0DpP5voom8MvV9BB6FcAjzN0Ro0bRRb\n' +
+        '-----END RSA PRIVATE KEY-----\n';
 
-	if(!req.headers.authorization) {
-		let err = {}
-		err.status = 401;
-		err.message = "authorization code not found!"
-		err.isFriendly = true;
-		next(err);
-	}
-
+    var url_parts = url.parse(req.url, true);
     var authorizationCode = req.headers.authorization.split(" ")[1];
     var buf;
-
     if (req.method == 'GET') {
-        buf = Buffer.from(authorizationCode, 'utf8');
+        buf = Buffer.from(authorizationCode + url_parts.search, 'utf8');
     }
     else {
         buf = Buffer.from(authorizationCode + req.body, 'utf8');
@@ -75,5 +68,5 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(3000, function () {
-    console.log("Listening at http://localhost:3000");
+    console.log("CodeFest API listening at http://localhost:3000");
 })
